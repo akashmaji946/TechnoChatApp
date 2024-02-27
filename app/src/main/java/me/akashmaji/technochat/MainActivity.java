723f09ea92b640456d3e1c38c73ecf0f307d0a1e
AKASH MAJI <akashmaji946@gmail.com>
2024-02-27 08:19:06 +0530

    Initial commit for main app bar layout

---
+++ app/src/main/java/me/akashmaji/technochat/MainActivity.java
@@ -0,0 +1,21 @@
+package me.akashmaji.technochat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Techno Chat");
+    }
+}
